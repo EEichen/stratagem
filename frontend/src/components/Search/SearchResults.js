77ce50f531b7612dd7ab94894b7a3af54dedc274
Elias Eichen <eliprof@live.com>
2021-04-28 10:43:00 -0500

    search stratagems click working

--- frontend/src/components/Search/SearchResults.js
+++ frontend/src/components/Search/SearchResults.js
@@ -1,18 +1,35 @@
 import { useSelector } from "react-redux"
+import { useHistory } from "react-router"
 
 const SearchResults = ({selectManual}) => {
+    let history = useHistory();
+
     const {
         foundManuals, 
         foundStratagems
     } = useSelector(store => store.searchResults)
 
+    const getStrat = (e) => {
+        if(e.target.id.startsWith('stratagem-')){
+            const id = e.target.id.split('-')[1]
+
+            history.push(`/stratagem/${id}`)
+
+        }
+    }
+
     return(
         <div>
             <div>
                 {foundStratagems[0] ? <div>Stratagems:</div> : ''}
-                <div className='search-strats'>
+                <div className='search-strats' onClick={getStrat}>
                     {foundStratagems.map(stratagem => (
-                        <div key={stratagem.id}>(placeholder) {stratagem.title}</div>
+                        <div 
+                        key={stratagem.id} 
+                        id={`stratagem-${stratagem.id}`}
+                        >
+                            (placeholder) {stratagem.title}
+                        </div>
                         ))}
                 </div>
             </div>
