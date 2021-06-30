a101d7b504e4baa1e45f6ac61c0e15111f4de7f4
Elias Eichen <eliprof@live.com>
2021-06-30 11:01:41 -0500

    added footer link logos for github and linked in

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,8 +1,12 @@
+import React from 'react'
+import ghLogo from '../../images/ghLogo.png'
+
 const Footer = () => {
     return(
         <div className='footer'>
             <span>
-                Created by: <a href="https://github.com/EEichen">Elias Eichen</a>
+                Created by: Elias Eichen <a className='gh-link' href="https://github.com/EEichen"><img src={ghLogo} alt="GitHub" /></a>
+            <span><a className='li-link' href="https://www.linkedin.com/in/elias-eichen-bb15a4198/"><i id='liico' class="fab fa-linkedin-in"></i></a></span>
             </span>
         </div>
     )
