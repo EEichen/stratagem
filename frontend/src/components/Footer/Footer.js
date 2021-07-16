916d249a8ba547b397e1afacc9c2be762554fc1a
Elias Eichen <eliprof@live.com>
2021-07-16 11:05:55 -0500

    in for LI link

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='footer'>
             <span>
                 Created by: Elias Eichen <a className='gh-link' href="https://github.com/EEichen"><img src={ghLogo} alt="GitHub" /></a>
-            <span><a className='li-link' href="https://www.linkedin.com/in/elias-eichen-bb15a4198/"><i id='liico' class="fab fa-linkedin-in"></i></a></span>
+            <span><a className='li-link' href="https://www.linkedin.com/in/elias-eichen-bb15a4198/">in</a></span>
             </span>
         </div>
     )
